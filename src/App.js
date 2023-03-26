b2d5b55464c22589b35da9bbc12b4a179b11f65a
DANCON1 <111096403+DANCON1@users.noreply.github.com>
2023-03-25 23:31:28 -0230

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Navigation from './Navigation';
+import SubmissionForm from './SubmissionForm';
+import Movies from './Movies';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+      <Router>
+        <Navigation />
+        <div className="container">
+          <Route path="/" element={<Movies />} />
+          <Route path="/submission" element={<SubmissionForm />} />
+        </div>
+      </Router>
+    );
 }
+
 
 export default App;
---
+++ src/SubmissionForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function SubmissionForm() {
+  return (
+    <div>
+      <h1>Submission Form</h1>
+      {
+        
+      }
+    </div>
+  );
+}
+
+export default SubmissionForm;
